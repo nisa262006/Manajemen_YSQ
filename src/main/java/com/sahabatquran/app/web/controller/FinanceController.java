e3fea205a52a7fe148079f93c246d2233c3f2553
unknown <davinaji0209@gmail.com>
2024-09-28 19:45:25 +0700

    Tampilan Finance

--- src/main/java/com/sahabatquran/app/web/controller/FinanceController.java
+++ src/main/java/com/sahabatquran/app/web/controller/FinanceController.java
@@ -1,4 +1,30 @@
 package com.sahabatquran.app.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/finance")
 public class FinanceController {
+
+    @GetMapping("/dashboard")
+    public String financeDashboard(){
+        return "finance/dashboard";
+    }
+
+    @GetMapping("/daftar-pembayaran")
+    public String daftarProgram(){
+        return "finance/daftar_pembayaran";
+    }
+
+    @GetMapping("/program-sedekah")
+    public String programSedekah(){
+        return "finance/program_sedekah";
+    }
+
+    @GetMapping("/daftar-sedekah")
+    public String daftarProgramSedekah(){
+        return "finance/daftar_sedekah";
+    }
 }
