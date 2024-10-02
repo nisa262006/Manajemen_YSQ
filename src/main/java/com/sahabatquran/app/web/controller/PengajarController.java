c4e80d7e2d79473214bcf73312a6cd2eb4cdbffe
riefkyarif <80331973+riefkyarif@users.noreply.github.com>
2024-10-02 18:29:51 +0700

    add controller related in pengajarController java

--- src/main/java/com/sahabatquran/app/web/controller/PengajarController.java
+++ src/main/java/com/sahabatquran/app/web/controller/PengajarController.java
@@ -15,6 +15,21 @@ public class PengajarController {
 
     @GetMapping("/daftar-kelas")
     public String pengajarDaftarKelas() {
-        return "pengajar/daftarKelas";
+        return "pengajar/daftar-kelas";
+    }
+
+    @GetMapping("/slip-gaji")
+    public String slipGaji(){
+        return "pengajar/slip-gaji";
+    }
+
+    @GetMapping("/nilai-ujian")
+    public String nilaiUjian() {
+        return "pengajar/nilai-ujian";
+    }
+
+    @GetMapping("/jadwal")
+    public String jadwalBelajar() {
+        return "pengajar/jadwal";
     }
 }
