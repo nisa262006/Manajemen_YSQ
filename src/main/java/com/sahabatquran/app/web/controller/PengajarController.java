01e3b5dc0e8d9f8bc22527da577ff770afa3cdcf
riefkyarif <80331973+riefkyarif@users.noreply.github.com>
2024-10-03 12:51:21 +0700

    put all controller needed

--- src/main/java/com/sahabatquran/app/web/controller/PengajarController.java
+++ src/main/java/com/sahabatquran/app/web/controller/PengajarController.java
@@ -2,6 +2,7 @@ package com.sahabatquran.app.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,13 +24,45 @@ public class PengajarController {
         return "pengajar/slip-gaji";
     }
 
-    @GetMapping("/nilai-ujian")
-    public String nilaiUjian() {
-        return "pengajar/nilai-ujian";
-    }
+    // Not in use -> included in sesi-belajar
+    // @GetMapping("/nilai-ujian")
+    // public String nilaiUjian() {
+    //     return "pengajar/nilai-ujian";
+    // }
 
     @GetMapping("/jadwal")
     public String jadwalBelajar() {
         return "pengajar/jadwal";
     }
+
+    @GetMapping("/sesi-belajar")
+    public String sesiBelajar() {
+        return "pengajar/sesi-belajar";
+    }
+
+    @GetMapping("/ujian-praktik")
+    public String ujianPraktik() {
+        return "pengajar/ujian-praktik";
+    }   
+
+    @GetMapping("/rekap-pengajaran")
+    public String rekapPengajaran() {
+        return "pengajar/rekap-pengajaran";
+    }   
+
+    @GetMapping("/rekap-ujian")
+    public String rekapUjian() {
+        return "pengajar/rekap-ujian";
+    }   
+
+    // Not used -> included in sesi-belajar
+    // @GetMapping("/mutabaah")
+    // public String mutabaah() {
+    //     return "pengajar/mutabaah";
+    // }   
+
+    @GetMapping("/berita-acara")
+    public String beritaAcara() {
+        return "pengajar/berita-acara";
+    }   
 }
