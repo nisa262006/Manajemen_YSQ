61ea8627a38d2086a9f914c50ff5a519191500db
Endy Muhardin <endy.muhardin@gmail.com>
2025-08-08 16:00:08 +0700

    fix failing test

---
+++ src/main/java/com/sahabatquran/app/web/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.sahabatquran.app.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
