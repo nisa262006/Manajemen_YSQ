d823654e10f937e072417f30b25b2b7102716a32
Endy Muhardin <endy.muhardin@gmail.com>
2025-08-08 13:03:18 +0700

    restart development using claude

--- src/main/resources/static/assets/js/password-toggler.js
+++
@@ -1,6 +0,0 @@
-$(".toggle-password").on("click" ,function (){
-    let el = $($(this).data("target"))
-    let type = el.prop("type")
-
-    el.prop('type', type === 'password' ? 'text': 'password')
-})
