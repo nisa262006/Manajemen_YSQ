8ef4b6443f8327bc438985d302444189cca3ac03
RahmaFitriaTunnisa <241572010009.tunnisa@student.stmik.tazkia.ac.id>
2025-11-26 09:56:28 +0700

    up nore

---
+++ Backend/src/config/db.js
@@ -0,0 +1,16 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "postgres",
+  port: process.env.DB_PORT || 5432,
+  password: process.env.DB_PASSWORD || "12345",
+  database: process.env.DB_NAME || "manajemen_db",
+});
+
+pool.connect()
+  .then(() => console.log("✅ PostgreSQL connected"))
+  .catch(err => console.error("❌ PostgreSQL error:", err.message));
+
+module.exports = pool;
