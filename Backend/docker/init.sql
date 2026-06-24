c90267addef8167520e32ca976f064acf2dbaaf1
RahmaFitriaTunnisa <241572010009.tunnisa@student.stmik.tazkia.ac.id>
2026-06-24 09:11:33 +0700

    fix: setup DB schema in CI before running tests, fix init.sql syntax errors

--- Backend/docker/init.sql
+++ Backend/docker/init.sql
@@ -366,10 +366,10 @@ INSERT INTO users (email, password_hash, role, status_user, username)
 VALUES ('admin5@ysq.com', '$2a$12$LlQBotZXQWe0ND5snff30eIrxjo4Q51j3lxX4S86h8m3GiIlv3L0C', 'admin', 'aktif', 'admin5');
 
 INSERT INTO users (email, password_hash, role, status_user, username)
-VALUES ('santri2@ysq.com',	'$2b$10$AGP.2LVxx8ykLYjiH0X8U.b6DHQyLv3O519up818wMVBW8w05J99a',	'santri',	'aktif'.	'YSQ26DWS012_santri2');
+VALUES ('santri2@ysq.com', '$2b$10$AGP.2LVxx8ykLYjiH0X8U.b6DHQyLv3O519up818wMVBW8w05J99a', 'santri', 'aktif', 'YSQ26DWS012_santri2');
 
 INSERT INTO users (email, password_hash, role, status_user, username)
-VALUES ('pengajar1@ysq.com',	'$2b$10$Dn47wRy1INoGPF1zu1VtmuU0Ic8dBhPRJQ9QSb2k6uGcd14pNxzWe',	'pengajar',	'aktif',	'YSQ25PGJ001_riska');
+VALUES ('pengajar1@ysq.com', '$2b$10$Dn47wRy1INoGPF1zu1VtmuU0Ic8dBhPRJQ9QSb2k6uGcd14pNxzWe', 'pengajar', 'aktif', 'YSQ25PGJ001_riska');
 
 --- admin
 INSERT INTO admin (id_users, nama, email)
@@ -387,34 +387,10 @@ VALUES (4, 'Admin4', 'admin4@ysq.com');
 INSERT INTO admin (id_users, nama, email)
 VALUES (5, 'Admin5', 'admin5@ysq.com');
 
-santri
-6	YSQ26DWS012	santri2	Dewasa	0812345678	santri2@ysq.com	bogor	2008-10-08	aktif	bogor	2026-01-08 15:19:35.254417+00
+--- santri
+INSERT INTO santri (id_users, nis, nama, kategori, no_wa, email, tempat_lahir, tanggal_lahir, status, alamat)
+VALUES (6, 'YSQ26DWS012', 'santri2', 'Dewasa', '0812345678', 'santri2@ysq.com', 'bogor', '2008-10-08', 'aktif', 'bogor');
 
-id_santri	12
-id_users	18
-nis	YSQ26DWS012
-nama	santri2
-kategori	Dewasa
-no_wa	0812345678
-email	santri2@ysq.com
-tempat_lahir	bogor
-tanggal_lahir	2008-10-08
-status	aktif
-alamat	bogor
-tanggal_terdaftar	2026-01-08 15:19:35.254417+00
-
-pengajar
-7	riska1	089876543210	Alamat Pengajar Baru 1778556757857	aktif		2006-10-06		pengajar1@ysq.com	YSQ25PGJ001	2025-12-23 09:23:54.387074+00
-
-id_pengajar	1
-id_users	5
-nama	riska1
-no_kontak	089876543210
-alamat	Alamat Pengajar Baru 1778556757857
-status	aktif
-tempat_lahir	
-tanggal_lahir	2006-10-06
-mapel	
-email	pengajar1@ysq.com
-nip	YSQ25PGJ001
-tanggal_terdaftar	2025-12-23 09:23:54.387074+00
+--- pengajar
+INSERT INTO pengajar (id_users, nama, no_kontak, alamat, status, tanggal_lahir, email, nip)
+VALUES (7, 'riska1', '089876543210', 'Alamat Pengajar Baru', 'aktif', '2006-10-06', 'pengajar1@ysq.com', 'YSQ25PGJ001');
